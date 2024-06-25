a4f2750fbcd93e1805974bbce966d520f7101f2c
wushumin <1780312291@qq.com>
2024-06-25 14:05:28 +0800

    VM-created

---
+++ VirtualMachine/src/main/java/org/hack/Main.java
@@ -0,0 +1,13 @@
+package org.hack;
+
+/**
+ * @Author: WuSM
+ * @description: TODO
+ * @Date: 2024-06-25 14:01
+ * @version: 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
